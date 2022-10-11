c4981fefbf1a739b41aa1ef2daffea423f5670a9
JanickiJ <kubajanicki244@gmail.com>
2022-10-11 14:59:51 +0200

    heroku

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import request
-
-from simpson import Simpson
+from app.simpson import Simpson
 
 app = Flask(__name__)
 Simpson = Simpson()
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "Welcome to counter app"
 
 
 @app.route('/count', methods=['GET'])
